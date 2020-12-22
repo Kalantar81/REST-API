9c1b1d12990e65b106567227acd48d304752e026
Kalantar81 <Calantarov@gmail.com>
2020-12-22 15:08:10 +0200

    implementation of main REST API methods on server and on client

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const path = require('path');
+// generates a new id's
+const {v4} = require('uuid');
 const app = express();
 
+let CONTACTS = [
+    {id: v4(), name: 'Vlad', value: '+7-921-100-20-30', marked: false}
+]
 
+app.use(express.json())
+
+// GET
+app.get('/api/contacts', (req, res) => {
+    setTimeout(() => {
+        res.status(200).json(CONTACTS);
+    }, 1000);
+    
+});
+
+// POST
+app.post('/api/contacts', (req, res) => {
+    // will be published on server
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+});
+
+// DELETE
+// id -param, that we pass for delete an item
+app.delete('/api/contacts/:id', (req, res) => {
+  CONTACTS = CONTACTS.filter(contact => contact.id !== req.params.id)
+  res.status(200).json({message: 'contact had be deleted'})
+});
+
+// PUT - update a specific element
+app.put('/api/contacts/:id', (req, res) => {
+    const index = CONTACTS.findIndex(contact => contact.id === req.params.id);
+    CONTACTS[index] = req.body;
+    res.status(200).json(CONTACTS[index])
+  });
 
 
 
--- client/frontend.js
+++ client/frontend.js
@@ -1,16 +1,25 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
 
+Vue.component('loader', {
+    template: `
+    <div style="display: flex; justify-content: center; align-items: center" >
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+    `
+})
+
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
             },
-            contacts: [
-                {id: 1, name: 'Vlad', value: '+7-921-100-20-30', marked: false}
-            ]
+            contacts: []
         }
     },
     computed: {
@@ -19,21 +28,61 @@ new Vue({
         }
     },
     methods: {
-        createContact() {
-           const {...contact} = this.form;
-           this.contacts.push({...contact, id: Date.now(), marked: false});
-           console.log(this.contacts);
+        async createContact() {
+            const { ...contact } = this.form;
+            
+            const newContact = await request('/api/contacts', 'POST', contact)
+            console.log(newContact)
 
-           this.form.name = '';
-           this.form.value = '';
+            this.contacts.push(newContact);
+
+            this.form.name = '';
+            this.form.value = '';
         },
-        markContact(id) {
+        async markContact(id) {
             const contact = this.contacts.find(c => c.id === id);
-            contact.marked = true;
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact,
+                marked: true
+            })
+            console.log(updated)
+            contact.marked = updated.marked;
         },
-        removeContact(id) {
-            this.contacts = this.contacts.filter((contact) => {contact.id !== id})
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE')
+            this.contacts = this.contacts.filter((contact) => { contact.id !== id })
         }
-    }
-    
+    },
+    // method, that called, when component is ready
+    async mounted() {
+        this.loading = true
+        // If server and client on different ports the url must be: 'http://localhost:3000/'
+        // contacts - the name of object, that we work with
+        this.contacts = await request('/api/contacts')
+        this.loading = false
+
+    },
+
 });
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        if (data) {
+            // format of data, that will be passed
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+        // fetch - async method, that enable to make an async requests
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch (error) {
+        console.log('Error: ', error.message)
+    }
+}
